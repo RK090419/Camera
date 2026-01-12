132027af0ac25f88b957efb6e3141b6c69b30330
RK090419 <renanaabu@gmail.com>
2026-01-12 14:28:39 +0200

    login background

--- R2000/R2000Wpf/Views/MainWindow.xaml.cs
+++ R2000/R2000Wpf/Views/MainWindow.xaml.cs
@@ -1,4 +1,6 @@
 ﻿using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Media;
 using R2000Wpf.ViewModels;
 
 namespace R2000Wpf.Views;
@@ -12,5 +14,18 @@ public partial class MainWindow : Window
     {
         DataContext = vm;
         InitializeComponent();
+        MainBorder.SizeChanged += OnSizeChanged;
+    }
+    private void OnSizeChanged(object? sender, SizeChangedEventArgs e)
+    {
+        if (sender is Border border)
+        {
+            border.Clip = new RectangleGeometry
+            {
+                Rect = new Rect(0, 0, border.ActualWidth, border.ActualHeight),
+                RadiusX = border.CornerRadius.TopLeft,
+                RadiusY = border.CornerRadius.TopLeft
+            };
+        }
     }
 }
